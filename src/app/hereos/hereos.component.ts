302abe147eb8d8a3fcb22e1e54a9ce4c992cf764
aboagyewaa1 <ewurama.amponsah@amalitech.com>
2023-03-21 13:39:15 +0000

    setting up

---
+++ src/app/hereos/hereos.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-hereos',
+  templateUrl: './hereos.component.html',
+  styleUrls: ['./hereos.component.css']
+})
+export class HereosComponent {
+
+}
