19c240ad1c9e114ef23f039da9672a1a1ce3c64e
aboagyewaa1 <ewurama.amponsah@amalitech.com>
2023-03-28 08:59:35 +0000

    nav

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HereosComponent } from './hereos/hereos.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path : 'heroes', component:HereosComponent},
+  {path:'dashboard',  component: DashboardComponent},
+  {path:'',redirectTo:'/dashboard',pathMatch:'full'},
+  {path:'detail/:id',component: HeroDetailComponent}
+];
+imports : [RouterModule.forRoot(routes)]
+exports: [RouterModule]
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/hereos/hereos.component.ts
+++ src/app/hereos/hereos.component.ts
@@ -1,10 +1,40 @@
-import { Component } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
+import { Hero } from '../hero';
+import { HEROES } from '../mock-hereos';
+import { HeroService } from '../hero.service';
+import { MessageService } from '../message.service';
 
 @Component({
   selector: 'app-hereos',
   templateUrl: './hereos.component.html',
   styleUrls: ['./hereos.component.css']
 })
-export class HereosComponent {
 
+export class HereosComponent implements OnInit {
+  heroes : Hero[] = [];
+
+
+  constructor(private heroService: HeroService){}
+ 
+
+
+getHeroes():void{
+  this.heroService.getHereos()
+      .subscribe(heroes => this.heroes = heroes);
+}
+ngOnInit():void{
+  this.getHeroes();
+}
+add(name: string): void {
+  name = name.trim();
+  if (!name) { return; }
+  this.heroService.addHero({ name } as Hero)
+    .subscribe(hero => {
+      this.heroes.push(hero);
+    });
+}
+delete(hero: Hero): void {
+  this.heroes = this.heroes.filter(h => h !== hero);
+  this.heroService.deleteHero(hero.id).subscribe();
+}
 }
